0bfa4c7c0173e638d679a50767377c1d5f22e040
J David Piérola A <jdpapiero@gmail.com>
2018-05-02 20:37:42 -0400

    Ajustes Estructura organizacional

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PositionTree} from '../shared/positionTree';
 import {PositionService} from '../services/position.service';
-import {API_URL, baseURL} from '../shared/baseurl';
-import {HttpHeaders} from '@angular/common/http';
 import {Location} from '@angular/common';
 
 @Component({
